aaacb1acf526e4ba71fa1296764c19d5abc0850e
Minh Nguyen <nguyendangbinhminh16@gmail.com>
2025-09-21 15:15:01 +0700

    Add provider and Variables for Iac

---
+++ terraform/core/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Terraform   = "true"
+      Owner       = "Minh Nguyen"
+      Project     = "Ephemeral AWS Infrastructure"
+    }
+  }
+}
